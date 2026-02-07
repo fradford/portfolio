1c656e44d6d0674ad862aa878c6ca90ddf9daaa5
Francis Radford <francis.radford@outlook.com>
2026-02-07 18:48:29 -0400

    fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,10 @@ module.exports = {
   apps: [
     {
       name: "portfolio",
-      exec_mode: "cluster",
-      instances: "1",
       script: "./node_modules/next/dist/bin/next",
       args: "start",
       exp_backoff_restart_delay: 100,
-      watch: true,
+      watch: false,
       max_memory_restart: "1G",
     },
   ],
